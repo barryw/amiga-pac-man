009ee77301749d17744c4593cbdb62e8461fb538
Barry Walker <barrywalker@gmail.com>
2024-11-19 21:03:02 -0500

    You're, like, uh-mazing dude

--- test-harnesses/MazeBorders2.h
+++ test-harnesses/MazeBorders2.h
@@ -18,6 +18,25 @@ typedef struct {
     int x2, y2; // End point
 } Line;
 
+typedef struct {
+    Point ul;
+    Point lr;
+} Oval;
+
+Point halfT[] = {
+    {14 * TILE_SIZE, 6 * TILE_SIZE + 4},
+    {14 * TILE_SIZE, 18 * TILE_SIZE + 4},
+    {14 * TILE_SIZE, 24 * TILE_SIZE + 4},
+};
+
+Oval ovals[] = {
+    {{2 * TILE_SIZE + 4, 2 * TILE_SIZE + 4}, {5 * TILE_SIZE + 3, 4 * TILE_SIZE + 3}},
+    {{7 * TILE_SIZE + 4, 2 * TILE_SIZE + 4}, {11 * TILE_SIZE + 3, 4 * TILE_SIZE + 3}},
+    {{2 * TILE_SIZE + 4, 6 * TILE_SIZE + 4}, {5 * TILE_SIZE + 3, 7 * TILE_SIZE + 3}},
+    {{7 * TILE_SIZE + 4, 15 * TILE_SIZE + 4}, {8 * TILE_SIZE + 3, 19 * TILE_SIZE + 3}},
+    {{7 * TILE_SIZE + 4, 21 * TILE_SIZE + 4}, {11 * TILE_SIZE + 3, 22 * TILE_SIZE + 3}},
+};
+
 Line leftSideLines[] = {
     // Ghost House
     {11 * TILE_SIZE, 13 * TILE_SIZE, 13 * TILE_SIZE, 13 * TILE_SIZE},
@@ -80,18 +99,24 @@ Line leftSideLines[] = {
     {0 * TILE_SIZE + 4, 30 * TILE_SIZE + 3, 0 * TILE_SIZE + 4, 30 * TILE_SIZE + 3},
     {0 * TILE_SIZE + 5, 30 * TILE_SIZE + 4, 14 * TILE_SIZE, 30 * TILE_SIZE + 4},
 
-    // Top left box
-    {2 * TILE_SIZE + 6, 2 * TILE_SIZE + 3, 5 * TILE_SIZE + 1, 2 * TILE_SIZE + 3},
-    {5 * TILE_SIZE + 1, 2 * TILE_SIZE + 3, 5 * TILE_SIZE + 3, 2 * TILE_SIZE + 5},
-    {5 * TILE_SIZE + 3, 2 * TILE_SIZE + 5, 5 * TILE_SIZE + 3, 4 * TILE_SIZE + 1},
-    {5 * TILE_SIZE + 3, 4 * TILE_SIZE + 1, 5 * TILE_SIZE + 1, 4 * TILE_SIZE + 3},
-    {5 * TILE_SIZE + 1, 4 * TILE_SIZE + 3, 2 * TILE_SIZE + 6, 4 * TILE_SIZE + 3},
-    {2 * TILE_SIZE + 6, 4 * TILE_SIZE + 3, 2 * TILE_SIZE + 4, 4 * TILE_SIZE + 1},
-    {2 * TILE_SIZE + 4, 4 * TILE_SIZE + 1, 2 * TILE_SIZE + 4, 2 * TILE_SIZE + 5},
-    {2 * TILE_SIZE + 4, 2 * TILE_SIZE + 5, 2 * TILE_SIZE + 6, 2 * TILE_SIZE + 3},
+    // L shape near bottom power pellet
+    {2 * TILE_SIZE + 6, 21 * TILE_SIZE + 4, 5 * TILE_SIZE + 1, 21 * TILE_SIZE + 4},
+    {5 * TILE_SIZE + 1, 21 * TILE_SIZE + 4, 5 * TILE_SIZE + 3, 21 * TILE_SIZE + 6},
+    {5 * TILE_SIZE + 3, 21 * TILE_SIZE + 6, 5 * TILE_SIZE + 3, 25 * TILE_SIZE + 1},
+    {5 * TILE_SIZE + 3, 25 * TILE_SIZE + 1, 5 * TILE_SIZE + 1, 25 * TILE_SIZE + 3},
+    {5 * TILE_SIZE + 1, 25 * TILE_SIZE + 3, 5 * TILE_SIZE - 1, 25 * TILE_SIZE + 3},
+    {5 * TILE_SIZE - 1, 25 * TILE_SIZE + 3, 5 * TILE_SIZE - 3, 25 * TILE_SIZE + 1},
+    {5 * TILE_SIZE - 3, 25 * TILE_SIZE + 1, 5 * TILE_SIZE - 3, 22 * TILE_SIZE + 5},
+    {5 * TILE_SIZE - 3, 22 * TILE_SIZE + 5, 5 * TILE_SIZE - 5, 22 * TILE_SIZE + 3},
+    {5 * TILE_SIZE - 5, 22 * TILE_SIZE + 3, 2 * TILE_SIZE + 6, 22 * TILE_SIZE + 3},
+    {2 * TILE_SIZE + 6, 22 * TILE_SIZE + 3, 2 * TILE_SIZE + 4, 22 * TILE_SIZE + 1},
+    {2 * TILE_SIZE + 4, 22 * TILE_SIZE + 1, 2 * TILE_SIZE + 4, 22 * TILE_SIZE - 1},
+    {2 * TILE_SIZE + 4, 22 * TILE_SIZE - 1, 2 * TILE_SIZE + 6, 21 * TILE_SIZE + 4},
 };
 
 int numLeftLines = sizeof(leftSideLines) / sizeof(Line);
+int numOvals = sizeof(ovals) / sizeof(Oval);
+int numHalfTs = sizeof(halfT) / sizeof(Point);
 
 inline void mirrorLines(const Line *leftLines, const int count, Line *mirroredLines) {
     const int centerX = MAZE_WIDTH / 2; // Axis of symmetry in pixels (center of the maze)
@@ -103,27 +128,105 @@ inline void mirrorLines(const Line *leftLines, const int count, Line *mirroredLi
     }
 }
 
+inline void genHalfT(Line *lines, const Point upperRight) {
+    int lineIndex = 0;
+
+    lines[lineIndex++] = (Line){upperRight.x, upperRight.y, upperRight.x - 3 * TILE_SIZE - 1, upperRight.y};
+    lines[lineIndex++] = (Line){
+        upperRight.x - 3 * TILE_SIZE - 1, upperRight.y, upperRight.x - 3 * TILE_SIZE - 3, upperRight.y + 2
+    };
+    lines[lineIndex++] = (Line){
+        upperRight.x - 3 * TILE_SIZE - 3, upperRight.y + 2, upperRight.x - 3 * TILE_SIZE - 3, upperRight.y + 4
+    };
+    lines[lineIndex++] = (Line){
+        upperRight.x - 3 * TILE_SIZE - 3, upperRight.y + 4, upperRight.x - 3 * TILE_SIZE - 1, upperRight.y + 6
+    };
+    lines[lineIndex++] = (Line){upperRight.x - 3 * TILE_SIZE - 1, upperRight.y + 6, upperRight.x - 5, upperRight.y + 6};
+    lines[lineIndex++] = (Line){upperRight.x - 5, upperRight.y + 6, upperRight.x - 3, upperRight.y + 8};
+    lines[lineIndex++] = (Line){upperRight.x - 3, upperRight.y + 8, upperRight.x - 3, upperRight.y + 3 * TILE_SIZE + 4};
+    lines[lineIndex++] = (Line){
+        upperRight.x - 3, upperRight.y + 3 * TILE_SIZE + 4, upperRight.x - 1, upperRight.y + 3 * TILE_SIZE + 6
+    };
+    lines[lineIndex] = (Line){
+        upperRight.x - 1, upperRight.y + 3 * TILE_SIZE + 6, upperRight.x, upperRight.y + 3 * TILE_SIZE + 6
+    };
+}
+
+inline void genOval(Line *lines, const Oval oval) {
+    int lineIndex = 0;
+    const Point topLeft = oval.ul;
+    const Point bottomRight = oval.lr;
+
+    lines[lineIndex++] = (Line){topLeft.x + 2, topLeft.y, bottomRight.x - 2, topLeft.y};
+    lines[lineIndex++] = (Line){topLeft.x + 2, bottomRight.y, bottomRight.x - 2, bottomRight.y};
+    lines[lineIndex++] = (Line){topLeft.x, topLeft.y + 2, topLeft.x, bottomRight.y - 2};
+    lines[lineIndex++] = (Line){bottomRight.x, topLeft.y + 2, bottomRight.x, bottomRight.y - 2};
+    lines[lineIndex++] = (Line){topLeft.x, topLeft.y + 2, topLeft.x + 2, topLeft.y};
+    lines[lineIndex++] = (Line){topLeft.x, bottomRight.y - 2, topLeft.x + 2, bottomRight.y};
+    lines[lineIndex++] = (Line){bottomRight.x - 2, topLeft.y, bottomRight.x, topLeft.y + 2};
+    lines[lineIndex] = (Line){bottomRight.x - 2, bottomRight.y, bottomRight.x, bottomRight.y - 2};
+}
+
 // Function to generate all maze lines
 inline void generateMazeLines(Line *combinedLines, int *totalLines) {
+    int currentIndex = 0;
+
     // Copy original left-side lines
     for (int i = 0; i < numLeftLines; i++) {
-        combinedLines[i] = leftSideLines[i];
+        combinedLines[currentIndex++] = leftSideLines[i];
+    }
+
+    // Half Ts (or full Ts when they're mirrored)
+    for (int i = 0; i < numHalfTs; i++) {
+        Line tLines[9];
+        genHalfT(tLines, halfT[i]);
+
+        for (int j = 0; j < 9; j++) {
+            combinedLines[currentIndex++] = tLines[j];
+        }
+
+        Line mirroredTLines[9];
+        mirrorLines(tLines, 9, mirroredTLines);
+
+        for (int j = 0; j < 9; j++) {
+            combinedLines[currentIndex++] = mirroredTLines[j];
+        }
+    }
+
+    // Add the ovals
+    for (int i = 0; i < numOvals; i++) {
+        Line ovalLines[8]; // Temporary storage for oval lines
+        genOval(ovalLines, ovals[i]);
+
+        // Append oval lines to combinedLines
+        for (int j = 0; j < 8; j++) {
+            combinedLines[currentIndex++] = ovalLines[j];
+        }
+
+        // Mirror the oval lines
+        Line mirroredOvalLines[8];
+        mirrorLines(ovalLines, 8, mirroredOvalLines);
+
+        // Append mirrored oval lines to combinedLines
+        for (int j = 0; j < 8; j++) {
+            combinedLines[currentIndex++] = mirroredOvalLines[j];
+        }
     }
 
     // Add mirrored lines
-    Line mirroredLines[numLeftLines];
-    mirrorLines(leftSideLines, numLeftLines, mirroredLines);
+    Line mirroredLeftLines[numLeftLines];
+    mirrorLines(leftSideLines, numLeftLines, mirroredLeftLines);
 
     for (int i = 0; i < numLeftLines; i++) {
-        combinedLines[numLeftLines + i] = mirroredLines[i];
+        combinedLines[currentIndex++] = mirroredLeftLines[i];
     }
 
-    *totalLines = 2 * numLeftLines;
+    *totalLines = currentIndex;
 }
 
 // Draw the maze on the RastPort
 inline void DrawMaze(struct RastPort *rp) {
-    Line combinedLines[2 * numLeftLines];
+    Line combinedLines[2 * numLeftLines + 16 * numOvals + 54];
     int totalLines;
 
     // Generate full maze lines
